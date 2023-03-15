424b8808a5cace7ebc3abe20d6549b8779a23adf
Attar74 <m.elattar.sev@gmail.com>
2023-03-15 13:24:34 +0200

    sideBar functionalities

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- cars-filter -->
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+    <!-- Location Start-->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Type</h3>
       <h3 class="text-blue-400 capitalize" @click="updateModal('location')"> {{ currentCity }} </h3>
@@ -14,15 +15,63 @@
         </button>
       </div>
     </div>
+    <!-- Location End-->
+    <!-- Make Start -->
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3>Make</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">  {{ route.params.make || 'Any' }} </h3>
+      <div v-if="modal.make" class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white w-[600px] flex justify-between flex-wrap">
+        <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">
+          {{ make }}
+        </h4>
+      </div>
+    </div>
+    <!-- Make End -->
+    <!-- Price Start -->
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3>Make</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')">  {{ priceRangeText }} </h3>
+      <div v-if="modal.price" class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+        <input class="border p-1 rounded my-1" type="number" placeholder="Min" v-model="priceRange.min" />
+        <input class="border p-1 rounded" type="number" placeholder="Max" v-model="priceRange.max" />
+        <span class="text-red-400" v-if="!isMaxGreaterThanMin&&isApplyPriceButtonHit"> Max price should be greater than Min</span>
+        <button class="bg-blue-400 w-full mt-2 rounded text-white" @click="onChangePrice()"> Apply </button>
+      </div>
+    </div>
+    <!-- Price End -->
   </div>
-  <!-- cars-filter -->
 </template>
 
 <script setup>
+const { makes } = useCars()
 
 const route = useRoute()
+const router = useRouter()
+
+let isApplyPriceButtonHit = ref(false)
+const isMaxGreaterThanMin = computed(() => {
+  return priceRange.value.max >= priceRange.value.min
+})
+const priceRangeText = computed(() => {
+  const max = route.query.maxPrice
+  const min = route.query.minPrice
+  if(!min && !max) return 'Any'
+  else if(!min && !max) {
+    return `< $${max}`
+  }
+  else if(min && !max) {
+    return `> $${min}`
+  } 
+  else {
+    return `$${min}-$${max}`
+  }
+})
 
 const city = ref('')
+const priceRange = ref({
+  min: '',
+  max: ''
+})
 
 const currentCity = ref(
   route.params.city
@@ -35,6 +84,13 @@ const modal = ref({
 })
 
 const updateModal = (key) => {
+  if(modal.value[key]){
+    modal.value[key] = false
+    return
+  }
+  for (const key in modal.value) { //to reset all modals before open one of them
+    colseModal(key)
+  }
   modal.value[key] = !modal.value[key]
 }
 
@@ -53,5 +109,25 @@ const onChangeLocation = () => {
   colseModal('location')
   navigateTo(`/city/${city.value}/car/${route.params.make}`)
   city.value = ""
+}
+
+const onChangeMake = (make) => {
+  colseModal('make')
+  navigateTo(`/city/${route.params.city}/car/${make}`)
+}
+
+const onChangePrice = () => {
+  if(priceRange.value.min <= priceRange.value.max) {
+    colseModal('price')
+    isApplyPriceButtonHit.value = false
+    router.push({
+      query: {
+        minPrice: priceRange.value.min,
+        maxPrice: priceRange.value.max
+      }
+    })
+  } else {
+    isApplyPriceButtonHit.value = true
   }
+}
 </script>
