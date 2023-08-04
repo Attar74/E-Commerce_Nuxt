56160e5b9ddcb02303ef6bf0edbc50ec6279fc71
Attar74 <m.elattar.sev@gmail.com>
2023-08-04 03:49:19 +0300

    adding listing creating items

---
+++ pages/profile/listings/view/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="rounded shadow mt-20">
+        <CarMessageCard />
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "custom"
+})
+
+const {listings} = useCars()
+</script>
